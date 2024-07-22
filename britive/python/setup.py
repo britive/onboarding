8518aec7254b65c37a546c7c65bb1448dac230ed
palakchheda <42711310+palakchheda@users.noreply.github.com>
2024-07-22 08:53:10 -0700

    starting britive/python

---
+++ britive/python/setup.py
@@ -0,0 +1,16 @@
+import time
+import random
+import string
+
+from colored import Fore, Back, Style
+from britive.britive import Britive
+
+br = Britive()
+
+
+def britive_setup():
+    print(f'hello Britive!')
+
+
+if __name__ == '__main__':
+    britive_setup()  # Invoke function to begin the password reset
